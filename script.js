8992c88cf5419db67ceef247829da7aad5d17245
Jarod Waggener <jarod2228@rocketmail.com>
2021-03-18 20:00:21 -0500

    finished studio, sans bonus mission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // TODO: add code here
+window.addEventListener("load", function () {
+    const astronauts = fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+    astronauts.then(response => response.json()
+    .then(json => {json.sort((a, b) => (b.hoursInSpace > a.hoursInSpace) ? 1 : -1);
+        const container = document.getElementById("container");
+        // let index = 0;
+        for (let i = 0; i < json.length; i++) {
+            container.innerHTML += `
+            <div class="astronaut">
+                <div class="bio">
+                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${json[i].hoursInSpace}</li>
+                        <li>Active: ${json[i].active}</li>
+                        <li>Skills: ${json[i].skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src=${json[i].picture}>
+                    </div>
+                    `
+                        }
+        }
+        ));
+});
