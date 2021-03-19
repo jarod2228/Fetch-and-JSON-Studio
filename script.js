8b15be54e09a3d22af70aff3777cab77b81b3dbd
Jarod Waggener <jarod2228@rocketmail.com>
2021-03-18 20:15:09 -0500

    fixed error

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 window.addEventListener("load", function () {
     const astronauts = fetch("https://handlers.education.launchcode.org/static/astronauts.json")
     astronauts.then(response => response.json()
-    .then(json => {json.sort((a, b) => (b.hoursInSpace > a.hoursInSpace) ? 1 : -1);
+    .then(json => 
         const container = document.getElementById("container");
-        // let index = 0;
+        {json.sort((a, b) => (b.hoursInSpace > a.hoursInSpace) ? 1 : -1);
         for (let i = 0; i < json.length; i++) {
             container.innerHTML += `
             <div class="astronaut">
@@ -12,14 +12,21 @@ window.addEventListener("load", function () {
                     <h3>${json[i].firstName} ${json[i].lastName}</h3>
                     <ul>
                         <li>Hours in space: ${json[i].hoursInSpace}</li>
-                        <li>Active: ${json[i].active}</li>
+                        <li id = "active">Active: ${json[i].active}</li>
                         <li>Skills: ${json[i].skills}</li>
                             </ul>
                         </div>
                         <img class="avatar" src=${json[i].picture}>
-                    </div>
-                    `
+                    </div>`
+                    }
+                    let activeVar = document.getElementsByClassName("active");
+                    for (let j = 0; j < json.length; j++) {
+                        if (json[j].active === true) {
+                            activeVar[j].style.color - "green";
+                        } else {
+                            activeVar[j].style.color = "red";
                         }
-        }
+                    }
+                        }
         ));
 });
